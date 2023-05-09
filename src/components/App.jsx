e425ce9fca411f41391ec7d2189405e50afd85d9
Ron Liu <itsronliu@gmail.com>
2023-05-09 13:19:28 -0700

    Refactor with react hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Hello world!</h1>
-      </div>
-    )
-  }
+const App = () => {
+  return (
+    <div>
+      <h1>Hello world!</h1>
+    </div>
+  )
 }
 
 export default App;
